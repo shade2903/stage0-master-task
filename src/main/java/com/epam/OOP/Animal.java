16783c65e3cfd082074c46f88520f55c1f93cca7
shade2903 <nighshade2903@gmail.com>
2023-04-26 16:44:59 +0300

    completed OOP

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -1,5 +1,29 @@
 package com.epam.OOP;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+    public String getDescription(){
+        return String.format("This animal is mostly %s. It has %d paw%s and %s fur.",color,numberOfPaws,
+                numberOfPaws> 1 ? "s": "", hasFur ? "a":"no");
+    }
 }
--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -1,5 +1,18 @@
 package com.epam.OOP;
 
-public class Bird {
+public class Bird extends Animal {
+    private int numberOfWings;
+
+    public Bird() {
+        super("blue", 2, false);
+        this.numberOfWings = 2;
+    }
+
+    @Override
+    public String getDescription() {
+        return  "This animal is mostly " + getColor() +". It has " + getNumberOfPaws()+" paw" +
+                (getNumberOfPaws()> 1 ? "s": "") + " and " + (isHasFur() ? "a":"no") + " fur. Moreover, it has " +
+                this.numberOfWings + " wings and can fly.";
+    }
 
 }
